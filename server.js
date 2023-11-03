c6d08810a21f530fcc6e1fead1f55b3789aa2448
kokosie <149806226+kokosie@users.noreply.github.com>
2023-11-03 20:32:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const ProtectedTextAPI = require("protectedtext-api");
 
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
 app.listen(PORT, () => {
   console.log(`app listening at http://localhost:${PORT}`);
